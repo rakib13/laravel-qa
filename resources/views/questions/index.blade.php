682e684ace3181e7886a7463e698dd2bb25a3090
rakib13 <rakib.omar13@gmail.com>
2021-06-29 08:33:06 +0600

    Build question form

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -5,26 +5,37 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">{{ __('All Questions') }}</div>
-    
+                    <div class="card-header">
+                        <div class="d-flex align-items-center">
+                            <h2>{{ __('All Questions') }}</h2>
+                            <div class="ml-auto">
+                                <a href="{{ route('questions.create') }}" class="btn btn-outline-secondary">
+                                    Ask Question
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
                     <div class="card-body">
                         @foreach ($questions as $question)
                             <div class="media">
                                 <div class="d-flex flex-column counters">
                                     <div class="vote">
-                                        <strong>{{ $question->votes }}</strong> {{Str::plural('vote', $question->votes)}}
+                                        <strong>{{ $question->votes }}</strong>
+                                        {{ Str::plural('vote', $question->votes) }}
                                     </div>
-                                    <div class="status {{$question->status}}">
-                                        <strong>{{ $question->answers }}</strong> {{Str::plural('answer', $question->answers)}}
+                                    <div class="status {{ $question->status }}">
+                                        <strong>{{ $question->answers }}</strong>
+                                        {{ Str::plural('answer', $question->answers) }}
                                     </div>
                                     <div class="view">
-                                    {{ $question->views . " " . Str::plural('view', $question->views)}}
+                                        {{ $question->views . ' ' . Str::plural('view', $question->views) }}
                                     </div>
                                 </div>
                                 <div class="media-body">
                                     <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
-                                    <p class="lead">Asked By 
-                                        <a href="{{ $question->user->url }}">{{$question->user->name}}</a>
+                                    <p class="lead">Asked By
+                                        <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
                                         <small class="text-muted">{{ $question->created_date }}</small>
                                     </p>
                                     {{ Str::limit($question->body, 250) }}
